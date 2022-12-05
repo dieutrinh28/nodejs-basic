4cfaa09610bd560bdedf68679866495ef92a1d5d
dieutrinh <trinhdo.work@gmail.com>
2022-12-05 18:37:32 +0700

    mvc-basic

--- src/route/web.js
+++ src/route/web.js
@@ -1,12 +1,11 @@
 import express from "express";
+import homeController from "../controllers/homeController";
 
 let router = express.Router();
 
 //app là express app; init mang nghĩa khởi tạo
 const initWebRoute = (app) => {
-    router.get('/', (req, res) => {
-        res.render('test/index.ejs')
-    })
+    router.get('/', homeController.getHomepage);
 
     router.get('/about', (req, res) => {
         res.send('I am Naibee!')
