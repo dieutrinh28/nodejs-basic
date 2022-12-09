f6d0c47ccb27c14bc3a6a564908f49648c118a62
dieutrinh <trinhdo.work@gmail.com>
2022-12-09 23:57:11 +0700

    connection-pool

--- src/configs/connectDB.js
+++ src/configs/connectDB.js
@@ -1,22 +1,12 @@
-// get the client
-import mysql from "mysql2";
+import mysql from "mysql2/promise";
 
 // create the connection to database
-const connection = mysql.createConnection({
+console.log("Creating connection pool...");
+
+const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
   database: 'nodejsbasic'
 });
 
-// simple query
-// connection.query(
-//   'SELECT * FROM `users` ',
-//   function(err, results, fields) {
-//     console.log('check mysql');
-//     console.log(results); // results contains rows returned by server
-//     let rows = results.map((row) => { return row.id })
-//     console.log(rows); // fields contains extra meta data about results, if available
-//   }
-// );
-
-export default connection;
+export default pool;
--- src/route/web.js
+++ src/route/web.js
@@ -6,6 +6,7 @@ let router = express.Router();
 //app là express app; init mang nghĩa khởi tạo
 const initWebRoute = (app) => {
     router.get('/', homeController.getHomepage);
+    router.get('/detail/user/:id', homeController.getDetailPage);
     router.get('/about', (req, res) => {
         res.send('I am Naibee!')
     })
