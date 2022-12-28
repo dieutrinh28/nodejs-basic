dfaae1fd4b5883c11541aff9b0653d61de9e842e
dieutrinh <trinhdo.work@gmail.com>
2022-12-28 12:35:26 +0700

    api-put-delete

--- src/controllers/APIController.js
+++ src/controllers/APIController.js
@@ -26,6 +26,35 @@ let createNewUser = async (req, res) => {
 
 }
 
+let updateUser = async (req, res) => {
+
+    let {firstName, lastName, email, address, id} = req.body;
+    if(!(firstName || lastName || email || address || id)) {
+        return res.status(200).json({
+            message: 'missing required params'
+        })
+    }
+    await pool.execute('UPDATE users SET firstName = ?, lastName = ?, email = ?, address = ? WHERE id = ?', 
+    [firstName, lastName, email, address, id]);
+
+    return res.status(200).json({
+        message: 'ok'
+    })
+}
+
+let deleteUser = async (req, res) => {
+    let userId = req.params.id;
+    if(!userId) {
+        return res.status(200).json({
+            message: 'missing required params'
+        })
+    }
+    await pool.execute('DELETE FROM users WHERE id = ?', [userId])
+    return res.status(200).json({
+        message: 'ok'
+    })
+}
+
 module.exports = {
-    getAllUsers, createNewUser
+    getAllUsers, createNewUser, updateUser, deleteUser
 }
--- src/route/api.js
+++ src/route/api.js
@@ -6,7 +6,9 @@ let router = express.Router();
 const initAPIRoute = (app) => {
     router.get('/users', APIController.getAllUsers);  
     router.post('/create', APIController.createNewUser); 
-   
+    router.put('/update', APIController.updateUser);
+    router.delete('/delete/:id', APIController.deleteUser);
+
     return app.use('/api/v1/', router)
 }
 
