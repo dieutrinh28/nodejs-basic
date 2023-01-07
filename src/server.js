2bab55e1c3dd1c2639542544f2388ce5065676b4
dieutrinh <trinhdo.work@gmail.com>
2023-01-07 16:42:39 +0700

    middleware

--- src/server.js
+++ src/server.js
@@ -6,15 +6,22 @@ import initWebRoute from "./route/web";
 import initAPIRoute from "./route/api";
 // import connection from "./configs/connectDB";
 
-require('dotenv').config();
+require("dotenv").config();
+var morgan = require("morgan");
 
+const app = express();
+const port = process.env.PORT || 8080;
 
-const app = express()
-const port = process.env.PORT;
+app.use((req, res) => {
+  console.log(">> run into my middleware");
+  console.log(req.method);
+  next();
+});
 
+app.use(morgan("combined"));
 //hỗ trợ gửi data từ phía client lên phía server, và có thể lấy data một cách đơn giản
 //giản lược hóa những tham số từ request gửi lên server
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // setup view engine
@@ -24,9 +31,14 @@ configViewEngine(app);
 initWebRoute(app);
 
 //init api route
-initAPIRoute(app); 
+initAPIRoute(app);
+
+//handle 404 not found
+app.use((req, res) => {
+  return res.render("404.ejs");
+});
 
 //lắng nghe cổng thực thi
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
